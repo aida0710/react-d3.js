bad166e52dfbefa28994a0e29d83ae3666114a65
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-08-17 13:05:25 +0900

    棒グラフを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import NetworkGraph from "./networkGraph";
+import NetworkGraph from "./NetworkGraph";
 
-function App() {
+const App = () => {
     return (
-        <div className="bg-amber-400 flex justify-center">
-            <div className="bg-white rounded-lg shadow-2xl p-4 m-4">
-                <h1 className="text-5xl">以下グラフ</h1>
-                <NetworkGraph/>
-            </div>
+        <div className="relative py-16">
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-white">アカウントにログイン</h1>
+            <NetworkGraph/>
         </div>
-    );
+    )
 }
 
 export default App;
