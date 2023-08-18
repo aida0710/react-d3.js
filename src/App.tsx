ed74af64b6775007a2d526f5be108b1e32a054e6
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-08-18 23:34:50 +0900

    svg領域がわかりやすいように軽く色づけ

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import NetworkGraph from "./NetworkGraph";
 
 const App = () => {
     return (
-        <NetworkGraph/>
+        <div className={'bg-red-500'}>
+            <div className={'text-xl text-white'}>
+                <p className={'p-8'}>背景が赤なのは上位div領域</p>
+            </div>
+
+            <div className={'bg-purple-950'}>
+                <div className={'text-xl text-white'}>背景が紫なのはsvg領域</div>
+                <NetworkGraph/>
+            </div>
+
+        </div>
     )
 }
 
