b8a6d954be0a7882df0501a55368da94f76b337a
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-08-18 18:57:12 +0900

    とりあえず変換してみた

---
+++ src/NetworkGraph.jsx
@@ -0,0 +1,125 @@
+import React, {Component} from 'react'
+import * as d3 from 'd3'
+import miserables from './miserables.json'
+
+class NetworkGraph extends Component {
+    componentDidMount() {
+        this.drawChart();
+    }
+
+    drawChart() {
+        const svg = d3.select("body"),
+            width = 800,
+            height = 800;
+
+        const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+        const simulation = d3
+            .forceSimulation()
+            .force(
+                "link",
+                d3.forceLink().id(function (d) {
+                    return d.id;
+                })
+            )
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+        const data = miserables;
+
+        const link = svg
+            .append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(data.links)
+            .enter()
+            .append("line")
+            .attr("stroke-width", function (d) {
+                return Math.sqrt(d.value);
+            });
+
+        const node = svg
+            .append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(data.nodes)
+            .enter()
+            .append("circle")
+            .attr("r", 5)
+            .attr("fill", function (d) {
+                return color(d.group);
+            })
+            .call(
+                d3
+                    .drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended)
+            )
+
+            .on("mouseover", function (e) {
+                console.log("ホバーしたよ");
+            })
+            .on("mouseout", function (e) {
+                console.log("離れたよ");
+            })
+            .on("click", function (e) {
+                console.log("クリックしたよ");
+            })
+
+        node.append("title").text(function (d) {
+            return d.id;
+        });
+
+        simulation.nodes(data.nodes).on("tick", ticked);
+
+        simulation.force("link").links(data.links);
+
+        function ticked() {
+            link
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node
+                .attr("cx", function (d) {
+                    return d.x;
+                })
+                .attr("cy", function (d) {
+                    return d.y;
+                });
+        }
+
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+    }
+
+    render() {
+        return <svg></svg>
+    }
+}
+
+export default NetworkGraph;
